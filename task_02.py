83fafbb8e56716ce42de935782f722503c7a8e85
rrafiringa <rrafiringa@gmail.com>
2015-12-05 14:11:26 +0000

    Task 02 corrections

--- task_02.py
+++ task_02.py
@@ -15,13 +15,27 @@ def get_cost_per_item(shoplist):
     Examples:
         >>> get_cost_per_item({'Black Plum': 8,
                                'Bosc Pear': 4})
-
+        {'Bosc Pear': 7.96, 'Black Plum': 23.92}
     """
     return {key: numfruits * FRUIT[key] for key,
-            numfruits in shoplist.iteritems()}
+            numfruits in shoplist.iteritems() if key in FRUIT.keys()}
+
+
+def get_total_cost(shoplist):
+    """
+    Args:
+        shoplist (dict): Shopping list
+    Returns:
+        Numeric: Total cost of all items
+    Examples:
+        >>> get_total_cost({'Black Plum': 8,
+                            'Bosc Pear': 4})
+    """
+    return sum(get_cost_per_item(shoplist).itervalues())
 
 
 if __name__ == '__main__':
     LIST = {'Black Plum': 8,
             'Bosc Pear': 4}
     print get_cost_per_item(LIST)
+    print get_total_cost(LIST)
